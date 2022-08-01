a532acd79e9f5ea336a0a39d4c3b4e753e96f291
huf <huf@aupup.com>
2022-08-01 15:30:50 +0800

    update

--- src/main/java/com/aupup/sso/client/Client3Application.java
+++ src/main/java/com/aupup/sso/client/Client3Application.java
@@ -1,61 +1,16 @@
 package com.aupup.sso.client;
 
-import com.aupup.auth.client.AuthClientConfiguration;
-import com.aupup.auth.client.jackson.ClientJackson2Module;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.security.jackson2.SecurityJackson2Modules;
 
 /**
- * @author c-nianyz
+ * @author huf
  **/
 @SpringBootApplication
-@Import({AuthClientConfiguration.class})
 public class Client3Application {
     public static void main(String[] args) {
         SpringApplication.run(Client3Application.class, args);
     }
 
-    @Bean(name = "redisTemplate")
-    @ConditionalOnMissingBean(name = "redisTemplate")
-    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<Object, Object> template = new RedisTemplate<>();
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setHashKeySerializer(new StringRedisSerializer());
-        template.setConnectionFactory(redisConnectionFactory);
-        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
-        jackson2JsonRedisSerializer.setObjectMapper(redisObjectMapper());
-        template.setValueSerializer(jackson2JsonRedisSerializer);
-        template.setHashValueSerializer(jackson2JsonRedisSerializer);
-        return template;
-    }
-    private ObjectMapper redisObjectMapper(){
-        ObjectMapper redisObjectMapper=new ObjectMapper();
-        redisObjectMapper.registerModule(new ClientJackson2Module());
-        redisObjectMapper.registerModules(SecurityJackson2Modules.getModules(getClass().getClassLoader()));
-        redisObjectMapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
-        return redisObjectMapper;
-    }
-    @Bean(name = {"redisCacheConfiguration"})
-    public RedisCacheConfiguration redisCacheConfiguration() {
-        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig();
-        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
-        jackson2JsonRedisSerializer.setObjectMapper(redisObjectMapper());
-        configuration = configuration
-                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jackson2JsonRedisSerializer))
-        ;
-        return configuration;
-    }
 }
--- src/main/java/com/aupup/sso/client/DemoController.java
+++ src/main/java/com/aupup/sso/client/DemoController.java
@@ -1,39 +1,20 @@
 package com.aupup.sso.client;
 
 
-import com.aupup.auth.client.JwtLoginUserComponent;
-import com.aupup.base.model.LoginUser;
+
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.WebSession;
 import reactor.core.publisher.Mono;
 
 
 @Slf4j
 @RestController
 public class DemoController {
-    @Autowired
-    private JwtLoginUserComponent jwtLoginUserComponent;
 
     @GetMapping("/user")
-    public Mono<Object> index(@AuthenticationPrincipal(expression = "loginUser") LoginUser loginUser) {
-            return Mono.just(loginUser);
-//        return loginUser.map(e->{
-////            Map<String,Object> principal = e.getAttribute("principal");
-////            List<Object> authorities = (List<Object>) principal.get("authorities");
-////            Map<String,Object> authority = (Map<String, Object>) authorities.get(0);
-////            Map<String,Object> attributes = (Map<String, Object>) authority.get("attributes");
-////            return attributes.get("loginUser");
-//            return e;
-//        });
+    public Mono<Object> index() {
+            return Mono.just("helloWorld");
     }
-    //jwt请求头获取当前用户
-    @GetMapping("/user1")
-    public Mono<Object> index(ServerHttpRequest request) {
-        return Mono.just(jwtLoginUserComponent.getCurrentUser(request));
-    }
+
 }
